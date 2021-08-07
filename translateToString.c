016b42832adb86e9d92517ce8542ee5900e8ba07
Darren Jennedy <djennedy@sfu.ca>
2021-08-07 16:05:40 -0700

    Fixed translate to string

--- translateToString.c
+++ translateToString.c
@@ -7,11 +7,14 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdint.h>
+#include <pwd.h>
+#include <bsd/string.h>
+#include <grp.h>
 
 #include "translateToString.h"
 #include "fileInfo.h"
 #include "infodemo.c"
-#include <pwd.h>
+
 
 
 char* getInodeString(ino_t inode)
@@ -21,58 +24,73 @@ char* getInodeString(ino_t inode)
 
 char* getPermissionString(mode_t mode)  // need to makee better
 {
-
     char* convertedString;
     strmode(mode,convertedString); // converts mode into symbolic string 
                             // stored in location referenced by convertetdString
-
-    printf("%11d", convertedString); // 11 spaces, right justified,
     
-    return convertedString;
+    return strdup(convertedString);
 }
 
 char* getHardLinksString(nlink_t nlink)
 {
     //nlink similar to size bc is a number too
-    intmax_t linkSize = (intmax_t) nlink; // casting it to "long" in everyones compuer
-    char* stringSize = sprintf("%jd", linkSize); //prints  output in char
+    intmax_t newLink = (intmax_t) nlink; // casting it to "long" in everyones compuer
+    char* stringLink;
+    if(sprintf(stringLink, "%jd", newLink)<0)
+    {
+        stringLink = "";
+    }
 
-    return strdup(stringSize); //duplicates string to count # of bytes
+    return strdup(stringLink); //duplicates string to count # of bytes
 }
 
 char* getOwnerString(uid_t owner)
 {
-    /*
-    //owner = numrical user ID
-    char* ownerName = (char*)owner; 
-    return strdup(ownerName);
-    */
+    struct passwd *pw = NULL;
+    pw = getpwuid(owner);
+    char* ownerString;
 
-    // getAndPrintUserName(owner);
-    return strdup( getAndPrintUserName(owner) ) ;
+    if(pw==NULL)
+    {
+        ownerString = "";
+    }
+    else
+    {
+        ownerString = pw->pw_name;
+    }
 
+    return strdup(ownerString);
 }
 
 char* getGroupString(gid_t group)
 {
-    /*
-    char* group = (char*)group; 
-    return strdup(group);
-    */
+    struct group *grp;
+    grp = getgrgid(group);
+    char* groupString;
 
-    //    getAndPrintGroup(group);
-    return strdup( getAndPrintGroup(group) );
+    if(grp ==NULL)
+    {
+        groupString = "";
+    }
+    else
+    {
+        groupString = grp->gr_name;
+    }
     
+    return strdup(groupString);
 }
 
 char* getByteSizeString(off_t size)
 {
     intmax_t newSize = (intmax_t) size; // casting it to "long"
                 // but intmax_t so its "long" in everyones compuer
-    char* stringSize = sprintf("%jd", newSize); //instead of printing to console
-                //stores output in char
+    char* byteSizeString;
+    if(sprintf(byteSizeString,"%jd", newSize)<0)
+    {
+        byteSizeString = "";
+    }
 
-    return strdup(stringSize); //duplicates string to count # of bytes, non-local
+    return strdup(byteSizeString); //duplicates string to count # of bytes, non-local
 }
 
 char* getDateString(struct timespec t)
