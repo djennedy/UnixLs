5e6dd5beee3592b8dcee86bfcce6c93432588bd5
Darren Jennedy <djennedy@sfu.ca>
2021-08-06 20:01:14 -0700

    simplified main.c

--- main.c
+++ main.c
@@ -8,107 +8,80 @@
 #include <string.h>
 #include <limits.h>
 
+#define NUM_OF_OPTIONS 3
+#define _i_FLAG 0
+#define _l_FLAG 1
+#define _R_FLAG 2
+
 int main(int argc, char const *argv[])
 {
-    bool hasFlags = false;
-
-    // It's better practice to change these to an array later on
-    bool i_flag = false;
-    bool l_flag = false;
-    bool R_flag = false;
-
-    // To check if we allocated memory in getcwd
-    bool needFree = false;
+    bool flags[NUM_OF_OPTIONS];
+    
+    int pathIndex = 1;
     
     // Check for options
     if(argc > 1)
     {
-        if(argv[1][0] == '-')
+        // Loop through argv
+        for (pathIndex=1;pathIndex<argc;pathIndex++)
         {
-            hasFlags = true;
-            int flagLength = strlen(argv[1]);
-            if(flagLength > 4 || flagLength ==1)
+            // Check if contains a flag
+            if(argv[pathIndex][0] == '-')
             {
-                printf("ERROR: Bad options");
-            }
+                int flagLength = strlen(argv[pathIndex]);
 
-            for (int i=1;i<flagLength;i++)
+                for (int j=1;j<flagLength;j++)
+                {
+                    if (argv[pathIndex][j] == 'i')
+                    {
+                        flags[_i_FLAG] = true;
+                    }
+                    else if (argv[pathIndex][j] == 'l')
+                    {
+                        flags[_l_FLAG] = true;
+                    }
+                    else if (argv[pathIndex][j] == 'R')
+                    {
+                        flags[_R_FLAG] = true;
+                    }
+                    else
+                    {
+                        printf("ERROR: Unknown option: %c for ls\n", argv[pathIndex][j]);
+                        return 0;
+                    }
+                }
+            }
+            // If no flag, break
+            else
             {
-                if (argv[1][i] == 'i')
-                {
-                    i_flag = true;
-                }
-                else if (argv[1][i] == 'l')
-                {
-                    l_flag = true;
-                }
-                else if (argv[1][i] == 'R')
-                {
-                    R_flag = true;
-                }
-                else
-                {
-                    printf("ERROR: Unknown option: %c for ls\n", argv[1][i]);
-                    return 0;
-                }
+                break;
             }
         }
     }
 
-    int numPaths=1;
-    if (hasFlags)
-    {
-        numPaths = argc > 2? argc-2 : 1;
-    }
-    else
-    {
-        numPaths = argc > 1? argc-1 : 1;
-    }
+    int numPaths = argc - pathIndex;
 
-    char* paths[numPaths];
+    char* paths[numPaths==0? 1 : numPaths];
 
     // Checks if no path is given
-    if(hasFlags && argc == 2 || !hasFlags && argc == 1)
+    if(numPaths==0)
     {
-        // Note: We're trying to avoid using PATH_MAX using this method
-
-        needFree = true;
-
-        paths[0] = getcwd(NULL, 0);
-        if(paths[0] == NULL)
-        {
-            printf("ERROR: getcwd failed\n");
-            return 0;
-        }
+        //Note: We're trying to avoid using PATH_MAX using this method
+        paths[0] = ".";
     }
     // If a path is given
     else 
     {
         // REMINDER: Remember that if we put more than 1 path argument, the path needs to be printed before listing (similar to -R)
 
-        if (hasFlags)
+        for(int i=pathIndex;i<argc;i++)
         {
-            for (int i=0;i<argc-2;i++)
-            {
-                paths[i] = argv[i+2];
-            }
-        }
-        else
-        {
-            for (int i=0;i<argc-1;i++)
-            {
-                paths[i] = argv[i+1];
-            }
+            paths[i-pathIndex] = argv[i];
         }
     }
 
     // TODO: do the ls stuff here
     
-
-    if(needFree)
-    {
-        free(paths[0]);
-    }
     return 0;
 }
 
